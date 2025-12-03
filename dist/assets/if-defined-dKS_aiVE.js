8c1eadccff2f6ebe5f0cbabf00c237d663a342da
zhizi1005 <zhzi1005@163.com>
2025-12-03 16:06:28 +0800

    1

--- dist/assets/if-defined-dKS_aiVE.js
+++
@@ -1,5 +0,0 @@
-import{m}from"./index-RXQWUp6b.js";/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const t=o=>o??m;export{t as o};
---
+++ dist/assets/transactions-BS9_4f6h.js
@@ -0,0 +1,16 @@
+import{w as f,u as a,x as m}from"./index-j_XmUmkn.js";import{c as p}from"./index-BZq1Q4eH.js";import"./index-DMaibNtU.js";import"./index-D1jKnbWH.js";import"./if-defined-BtgLxx5f.js";import"./index--SsCp6xV.js";import"./index-Ch0wcyIE.js";import"./index-DGEQF1cx.js";const u=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var w=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=u;s=w([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
