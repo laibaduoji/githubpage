ce564acf233ba52917ab94a1e31938efeb8a9ba8
zhizi1005 <zhzi1005@163.com>
2025-12-03 22:20:14 +0800

    1

---
+++ dist/assets/transactions-CiPrWwe_.js
@@ -0,0 +1,16 @@
+import{w as f,u as a,x as m}from"./index-Bbs1TI2A.js";import{c as p}from"./index-CEWJ_qGb.js";import"./index-CsAcL1vD.js";import"./index-CZhMXMw7.js";import"./if-defined-CGR0FP-o.js";import"./index-C55HRxvD.js";import"./index-DMLQBywW.js";import"./index-zHjekLRn.js";const u=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var w=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=u;s=w([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
